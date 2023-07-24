551a06364d471ba473fdba1efd21a064cd13d33e
shingggk <97640959+shingggk@users.noreply.github.com>
2023-07-24 02:28:40 +0000

    PE01 submission

---
+++ PE01-Resume/my-app/src/Resume.js
@@ -0,0 +1,46 @@
+
+function Resume() {
+  return (
+    <>
+      <h1>Studnet Name</h1>
+      <p>521 Wall St #100, Seattle, WA 98121</p>
+      <h2>Education</h2>
+      <h3>Master of Science in Computer Science</h3>
+      <p>City University of Seattle, Seattle WA | May 2023
+        <br/>
+        GPA: 4.0
+      </p>
+      <h3>Bachelor of Science in General Science</h3>
+      <p>University of Oregon, Eugene OR | May 2018
+        <br/>
+        GPA: 3.7
+      </p>
+      <h2>Skills</h2>
+      <p>Programming Language: JavaScript, Python 
+        <br/><br/>
+        Frameworks and libraries: React, Next.js, Node.js, Express
+        <br/><br/>
+        Database: MySQL, MongoDB
+        <br/><br/>
+        Version Control: Git
+      </p>
+      <h2>Work Experience</h2>
+      <h3>Software Development Intern</h3>
+      <p>One Code Club, Seattle, WA | April, 2023 - Present
+      <br/><br/>
+        Worked as a frontend developer at a non-profit organization
+      </p>
+      <h3>Graduate Assistant</h3>
+      <p>City University of Seattle, Seattle, WA | April, 2022 - Present
+      <br/><br/>
+        Worked as a graduate student assistant to maintain school's education management system
+      </p>
+      <h2>Projects</h2>
+      <h3>One Code Club Homepage</h3>
+      <p>https://example.com</p>
+
+    </>
+  );
+}
+
+export default Resume;
